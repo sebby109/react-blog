f084896449c5c154593cfe588fb16abc70a561be
sebby109 <sebastianga14@hotmail.com>
2024-02-06 12:13:44 -0700

    custom navbar made and completed

--- react-blog/src/Components/NavB.jsx
+++ react-blog/src/Components/NavB.jsx
@@ -1,22 +1,18 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import '../Styles/NavS.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import home from '../Images/icons8-home-100.png';
+import friends from '../Images/icons8-friends-64.png';
+import profile from '../Images/icons8-cat-profile-64.png';
+import { Image } from 'react-bootstrap';
 
 function NavB(){
     
     return (
-        <Navbar data-bs-theme="dark" fixed='bottom' id='nBar'>
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <footer>
+        <a href="#"><Image src={home} /></a>
+        <a href="#"><Image src={friends} /></a>
+        <a href="#"><Image src={profile} /></a>
+      </footer>
     );
 }
 
