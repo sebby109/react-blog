8a66229ac9214538ce747cd1ce65b5d5091ba2b4
sebby109 <sebastianga14@hotmail.com>
2024-02-24 17:45:17 -0700

    finished friends list page, currently has mock friends. Will now start backend of website

--- react-blog/src/Components/UserPhotos.jsx
+++ react-blog/src/Components/UserPhotos.jsx
@@ -2,16 +2,30 @@
 import '../Styles/UserPhotos.css';
 import NavB from './NavB';
 import { useState } from "react";
+import generic from '../Images/icons8-male-user-94.png';
+import { Image } from 'react-bootstrap';
 
+/**
+ * hide in the NavB makes it not show the post box used in small screens,
+ * that should only appear when their is a feed to post to.
+ */
 function UserPhotos(){
-    
     const [data, setData] = useState(Array.from({length:20}))
 
     return(
         <div className="parent-photos">
             <NavB hide={true}/>
             <div className="photos-child">
-                hello
+            {data.map(
+                    (item, index) => {
+                        return(
+                            <div className='photos-children'>
+                                Name
+                                <Image src={generic} />
+                            </div>
+                        )
+                    }
+                )}
             </div>
         </div>
     );
