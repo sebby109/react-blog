4f27d9b35e940b542efc1d6485a84e215cecb50f
sebby109 <sebastianga14@hotmail.com>
2024-03-14 18:30:03 -0700

    define env in vite config

--- react-blog/src/Communication/supabaseClient.js
+++ react-blog/src/Communication/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_KEY;
 const supabaseKey = process.env.REACT_APP_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
